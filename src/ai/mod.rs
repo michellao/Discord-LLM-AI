488187247cce02b6111786051a0b67feb7123bd4
Michel LAO <SansNom01@proton.me>
2024-04-26 14:32:16 +0200

    Override default options model

--- src/ai/mod.rs
+++ src/ai/mod.rs
@@ -1,4 +1,4 @@
-use ollama_rs::generation::{completion::request::GenerationRequest, parameters::{KeepAlive, TimeUnit}};
+use ollama_rs::generation::{completion::request::GenerationRequest, options::GenerationOptions, parameters::{KeepAlive, TimeUnit}};
 
 pub struct GenerationTemplate {
     model_name: String,
@@ -13,6 +13,12 @@ impl GenerationTemplate {
 
     pub fn template(&self, prompt: String) -> GenerationRequest {
         let generation_request = GenerationRequest::new(self.model_name.to_string(), prompt);
-        generation_request.keep_alive(KeepAlive::Until { time: 300, unit: TimeUnit::Minutes })
+        let options = GenerationOptions::default()
+            .temperature(0.6)
+            .num_predict(256)
+            .num_ctx(8192);
+        generation_request
+            .keep_alive(KeepAlive::Until { time: 300, unit: TimeUnit::Minutes })
+            .options(options)
     }
 }
