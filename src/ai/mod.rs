615d2c1091d82fae3c6b5e00804893c29fc1f055
Michel LAO <SansNom01@proton.me>
2024-05-02 16:25:11 +0200

    Refactor AI

--- src/ai/mod.rs
+++ src/ai/mod.rs
@@ -1,24 +1,42 @@
-use ollama_rs::generation::{completion::request::GenerationRequest, options::GenerationOptions, parameters::{KeepAlive, TimeUnit}};
+use ollama_rs::{generation::{completion::request::GenerationRequest, options::GenerationOptions, parameters::{KeepAlive, TimeUnit}}, Ollama};
+use tokio::sync::Mutex;
 
-pub struct GenerationTemplate {
+pub struct GenerationAI {
     model_name: String,
+    ollama: Mutex<Ollama>
 }
 
-impl GenerationTemplate {
-    pub fn new(model_name: String) -> GenerationTemplate {
-        GenerationTemplate {
-            model_name
+pub trait TextGeneration {
+    async fn generate(&self, prompt: String) -> String;
+}
+
+impl GenerationAI {
+    pub fn new(model_name: String, host: String, port: u16) -> Self {
+        Self {
+            model_name,
+            ollama: Mutex::new(Ollama::new(host, port))
         }
     }
 
-    pub fn template(&self, prompt: String) -> GenerationRequest {
+    fn template_ollama_request(&self, prompt: String) -> GenerationRequest {
         let generation_request = GenerationRequest::new(self.model_name.to_string(), prompt);
         let options = GenerationOptions::default()
             .temperature(0.6)
-            .num_predict(400)
+            .num_predict(384)
             .num_ctx(8192);
         generation_request
             .keep_alive(KeepAlive::Until { time: 300, unit: TimeUnit::Minutes })
             .options(options)
+    }
+}
+
+impl TextGeneration for GenerationAI {
+    async fn generate(&self, prompt: String) -> String {
+        let ollama = self.ollama.lock().await;
+        let res = ollama.generate(self.template_ollama_request(prompt)).await;
+        match res {
+            Ok(data) => data.response,
+            Err(_) => String::from("Error response")
+        }
     }
 }
