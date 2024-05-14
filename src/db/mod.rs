12caa60281aba19e88e07f021b45911fc4c4d5e7
Michel LAO <SansNom01@proton.me>
2024-05-14 10:00:50 +0200

    Update relationnal database

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -8,15 +8,19 @@ impl Database {
     pub fn new() -> Self {
         let conn = Connection::open_in_memory().unwrap();
         conn.execute(
-            "CREATE TABLE user_ai (
-                user_id INTEGER PRIMARY KEY,
+            "CREATE TABLE user (
+                id_user INTEGER PRIMARY KEY,
+                is_bot INTEGER NOT NULL DEFAULT FALSE,
+                discord_id INTEGER DEFAULT NULL
             );",
             ()
         ).unwrap();
         conn.execute(
             "CREATE TABLE message (
                 id_message INTEGER PRIMARY KEY,
-                content TEXT NOT NULL
+                user_id INTEGER,
+                content TEXT NOT NULL,
+                FOREIGN KEY (user_id) REFERENCES user(id_user)
             )",
             ()
         ).unwrap();
