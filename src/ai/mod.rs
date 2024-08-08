43117ddc6a21abd9e1f2bea9b05e2e099af7c59a
Alexis Lao <SansNom01@proton.me>
2024-08-08 19:08:28 +0200

    Update GenerationAI for llama cpp

--- src/ai/mod.rs
+++ src/ai/mod.rs
@@ -1,9 +1,8 @@
-use ollama_rs::{generation::{completion::request::GenerationRequest, options::GenerationOptions, parameters::{KeepAlive, TimeUnit}}, Ollama};
+use inference_ai::InferenceAI;
 use tokio::sync::Mutex;
 
 pub struct GenerationAI {
-    model_name: String,
-    ollama: Mutex<Ollama>
+    llamacpp: Mutex<InferenceAI>
 }
 
 pub trait TextGeneration {
@@ -13,30 +12,17 @@ pub trait TextGeneration {
 impl GenerationAI {
     pub fn new(model_name: String, host: String, port: u16) -> Self {
         Self {
-            model_name,
-            ollama: Mutex::new(Ollama::new(host, port))
+            llamacpp: Mutex::new(InferenceAI::new(model_name, host, port)),
         }
-    }
-
-    fn template_ollama_request(&self, prompt: String) -> GenerationRequest {
-        let generation_request = GenerationRequest::new(self.model_name.to_string(), prompt);
-        let options = GenerationOptions::default()
-            .temperature(0.6)
-            .num_predict(390)
-            .num_ctx(8192);
-        generation_request
-            .keep_alive(KeepAlive::Until { time: 300, unit: TimeUnit::Minutes })
-            .options(options)
     }
 }
 
 impl TextGeneration for GenerationAI {
     async fn generate(&self, prompt: String) -> String {
-        let ollama = self.ollama.lock().await;
-        let res = ollama.generate(self.template_ollama_request(prompt)).await;
-        match res {
-            Ok(data) => data.response,
-            Err(_) => String::from("Error response")
+        let mut llamacpp = self.llamacpp.lock().await;
+        match llamacpp.completion(&prompt).await {
+            None => String::from("Error completion"),
+            Some(response) => response
         }
     }
 }
