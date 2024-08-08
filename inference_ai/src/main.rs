5e8017619c2f327e40aa517da4ba4e5da39326a9
Alexis Lao <SansNom01@proton.me>
2024-08-08 19:12:49 +0200

    Update param

--- inference_ai/src/main.rs
+++ inference_ai/src/main.rs
@@ -2,7 +2,7 @@ use inference_ai::InferenceAI;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut inference_ai = InferenceAI::new("http://localhost".to_string(), 8080);
+    let mut inference_ai = InferenceAI::new(String::from("llama-3.1"), String::from("http://localhost"), 8080);
     inference_ai.completion("new_text").await;
     Ok(())
 }
