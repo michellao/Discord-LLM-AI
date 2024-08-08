77265147ee3f40b0e03f8acecaba39da88be5002
Alexis Lao <SansNom01@proton.me>
2024-08-08 19:06:34 +0200

    Begin implementation database select is working

--- database/src/lib.rs
+++ database/src/lib.rs
@@ -1,12 +1,13 @@
+use std::{fmt::format, vec};
+
 use rusqlite::Connection;
 
 pub struct Database {
-    conn: Connection
+    conn: Connection,
 }
 
 impl Database {
-    pub fn new() -> Self {
-        let conn = Connection::open_in_memory().unwrap();
+    pub fn new(conn: Connection) -> Self {
         conn.execute(
             "CREATE TABLE user (
                 id_user INTEGER PRIMARY KEY,
@@ -29,11 +30,92 @@ impl Database {
         }
     }
 
-    pub fn delete(&self) {
-        self.conn.execute("
-            DROP TABLE message;
-            ",
+    fn transform_rows_to_string(table_names: &Vec<&str>) -> String {
+        let mut rows_string = String::from("");
+        for i in 0..table_names.capacity() {
+            let string: String;
+            if table_names.capacity() - 1 > i {
+                string = format!("{}, ", table_names[i]);
+            } else {
+                string = format!("{}", table_names[i]);
+            }
+            rows_string.push_str(&string);
+        }
+        rows_string
+    }
+
+    pub fn select(&self, object: &impl Data) {
+        let row_names = object.to_obj();
+        let rows_string = Self::transform_rows_to_string(&row_names);
+        let sql = format!("SELECT {} FROM {};", rows_string, object.to_table_name());
+        println!("{sql}");
+        let result = self.conn.execute(
+            &sql,
             ()
-        ).unwrap();
+        );
+        println!("{:?}", result);
+    }
+
+    pub fn insert(&self, object: &impl Data) {
+        let row_names = object.to_obj();
+        let sql = format!("INSERT INTO {} VALUES ()", object.to_table_name());
+        let result = self.conn.execute(
+            &sql,
+        ()
+        );
+    }
+
+    pub fn delete(&self, object: &impl Data, primary_id: &i64) -> bool {
+        let first_row = object.to_obj()[0];
+        let sql = format!("DELETE FROM {} WHERE {} = {}", object.to_table_name(), first_row, primary_id);
+        let result = self.conn.execute(
+            &sql,
+            ()
+        );
+        match result {
+            Err(_) => false,
+            Ok(value) => value > 0
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct User {
+    pub id_user: u64,
+    pub is_bot: bool,
+    pub discord_id: u64
+}
+
+#[derive(Debug)]
+pub struct Message {
+    pub id_message: u64,
+    pub user: User,
+    pub content: String
+}
+
+pub trait Data {
+    fn to_table_name<'a>(&self) -> &'a str;
+    fn to_obj(&self) -> Vec<&str>;
+}
+
+impl Data for User {
+
+    fn to_table_name<'a>(&self) -> &'a str {
+        "user"
+    }
+    
+    fn to_obj(&self) -> Vec<&str> {
+        vec!["id_user", "is_bot" , "discord_id"]
+    }
+}
+
+impl Data for Message {
+
+    fn to_table_name<'a>(&self) -> &'a str {
+        "message"
+    }
+    
+    fn to_obj(&self) -> Vec<&str> {
+        vec!["id_message", "user", "content"]
     }
 }
