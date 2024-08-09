6d56df86c787d0b3e8c919f1a45a518a134d7ddc
Alexis Lao <SansNom01@proton.me>
2024-08-09 19:35:20 +0200

    Handling large messages more than 2000 characters to be compliant with Discord API

--- src/main.rs
+++ src/main.rs
@@ -34,18 +34,46 @@ async fn text(
         poise::CreateReply::default()
             .content("Generating...")
     };
-    let handle_response = ctx.send(reply).await?;
+    let handle_generating_message = ctx.send(reply).await?;
 
     let generation_ai = &ctx.data().generation_ai;
     let response = generation_ai.generate(prompt).await;
+    println!("Complete response: {}", response);
+    let lenght_response = response.len();
+    let character_limit = 2000;
+    let mut complete_response: Vec<&str> = vec![];
+    if lenght_response > character_limit {
+        let mut response_to_divide = response.as_str();
+        let nb_divide = lenght_response / character_limit;
+        for _i in 0..nb_divide {
+            let split_response = response_to_divide.split_at(character_limit);
+            complete_response.push(split_response.0);
+            response_to_divide = split_response.1;
+        }
+        complete_response.push(&response_to_divide);
+    } else {
+        complete_response.push(response.as_str());
+    }
 
-    println!("response: {}", response);
+    println!("Separate response: {:?}", complete_response);
 
-    handle_response.edit(
-        ctx,
-        poise::CreateReply::default()
-            .content(response)
-    ).await?;
+    for answer in 0..complete_response.len() {
+        println!("Answer {}", answer);
+        if answer == 0 {
+            handle_generating_message.edit(
+                ctx,
+                poise::CreateReply::default()
+                    .content(complete_response[answer])
+            ).await?;
+        } else {
+            let message = {
+                poise::CreateReply::default()
+                    .content(complete_response[answer])
+            };
+            ctx.send(message).await?;
+        }
+    }
+
     Ok(())
 }
 
