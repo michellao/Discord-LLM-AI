2ff709aa0ed930b811a321666335609ca5170227
Alexis Lao <SansNom01@proton.me>
2024-08-10 17:36:39 +0200

    Remove trait TextGeneration

--- src/ai/mod.rs
+++ src/ai/mod.rs
@@ -5,20 +5,14 @@ pub struct GenerationAI {
     llamacpp: Mutex<InferenceAI>
 }
 
-pub trait TextGeneration {
-    async fn generate(&self, prompt: String) -> String;
-}
-
 impl GenerationAI {
     pub fn new(model_name: String, host: String, port: u16) -> Self {
         Self {
             llamacpp: Mutex::new(InferenceAI::new(model_name, host, port)),
         }
     }
-}
 
-impl TextGeneration for GenerationAI {
-    async fn generate(&self, prompt: String) -> String {
+    pub async fn generate(&self, prompt: String) -> String {
         let mut llamacpp = self.llamacpp.lock().await;
         match llamacpp.completion(&prompt).await {
             None => String::from("Error completion"),
