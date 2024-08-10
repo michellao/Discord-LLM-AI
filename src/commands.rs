e67ee9af265a9359cacf52549161ff1ceb6b251a
Alexis Lao <SansNom01@proton.me>
2024-08-10 17:37:11 +0200

    Separate commands on Discord in a separate file

---
+++ src/commands.rs
@@ -0,0 +1,54 @@
+use crate::{Context,Error};
+
+/// Response by an AI
+#[poise::command(slash_command)]
+pub async fn text(
+    ctx: Context<'_>,
+    #[description = "Prompt"] prompt: String,
+) -> Result<(), Error> {
+    let reply = {
+        poise::CreateReply::default()
+            .content("Generating...")
+    };
+    let handle_generating_message = ctx.send(reply).await?;
+
+    let generation_ai = &ctx.data().generation_ai;
+    let response = generation_ai.generate(prompt).await;
+    println!("Complete response: {}", response);
+    let lenght_response = response.len();
+    let character_limit = 2000;
+    let mut complete_response: Vec<&str> = vec![];
+    if lenght_response > character_limit {
+        let mut response_to_divide = response.as_str();
+        let nb_divide = lenght_response / character_limit;
+        for _i in 0..nb_divide {
+            let split_response = response_to_divide.split_at(character_limit);
+            complete_response.push(split_response.0);
+            response_to_divide = split_response.1;
+        }
+        complete_response.push(response_to_divide);
+    } else {
+        complete_response.push(response.as_str());
+    }
+
+    println!("Separate response: {:?}", complete_response);
+
+    for answer in 0..complete_response.len() {
+        println!("Answer {}", answer);
+        if answer == 0 {
+            handle_generating_message.edit(
+                ctx,
+                poise::CreateReply::default()
+                    .content(complete_response[answer])
+            ).await?;
+        } else {
+            let message = {
+                poise::CreateReply::default()
+                    .content(complete_response[answer])
+            };
+            ctx.send(message).await?;
+        }
+    }
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 mod ai;
+mod commands;
 
 use database::Database;
 use dotenv::dotenv;
-use ai::{GenerationAI, TextGeneration};
+use ai::GenerationAI;
 use poise::serenity_prelude as serenity;
 use rusqlite::Connection;
+
+pub struct DataDiscord {
+    generation_ai: GenerationAI,
+}
+
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, DataDiscord, Error>;
 
 fn setup_ai() -> GenerationAI {
     let host = std::env::var("LLAMACPP_HOST").expect("missing LLAMACPP_HOST");
@@ -15,66 +23,6 @@ fn setup_ai() -> GenerationAI {
         Err(_) => panic!("Invalid port")
     };
     GenerationAI::new(text_model, host, port)
-}
-
-
-struct Data {
-    generation_ai: GenerationAI,
-}
-type Error = Box<dyn std::error::Error + Send + Sync>;
-type Context<'a> = poise::Context<'a, Data, Error>;
-
-/// Response by an AI
-#[poise::command(slash_command)]
-async fn text(
-    ctx: Context<'_>,
-    #[description = "Prompt"] prompt: String,
-) -> Result<(), Error> {
-    let reply = {
-        poise::CreateReply::default()
-            .content("Generating...")
-    };
-    let handle_generating_message = ctx.send(reply).await?;
-
-    let generation_ai = &ctx.data().generation_ai;
-    let response = generation_ai.generate(prompt).await;
-    println!("Complete response: {}", response);
-    let lenght_response = response.len();
-    let character_limit = 2000;
-    let mut complete_response: Vec<&str> = vec![];
-    if lenght_response > character_limit {
-        let mut response_to_divide = response.as_str();
-        let nb_divide = lenght_response / character_limit;
-        for _i in 0..nb_divide {
-            let split_response = response_to_divide.split_at(character_limit);
-            complete_response.push(split_response.0);
-            response_to_divide = split_response.1;
-        }
-        complete_response.push(response_to_divide);
-    } else {
-        complete_response.push(response.as_str());
-    }
-
-    println!("Separate response: {:?}", complete_response);
-
-    for answer in 0..complete_response.len() {
-        println!("Answer {}", answer);
-        if answer == 0 {
-            handle_generating_message.edit(
-                ctx,
-                poise::CreateReply::default()
-                    .content(complete_response[answer])
-            ).await?;
-        } else {
-            let message = {
-                poise::CreateReply::default()
-                    .content(complete_response[answer])
-            };
-            ctx.send(message).await?;
-        }
-    }
-
-    Ok(())
 }
 
 #[tokio::main]
@@ -94,13 +42,13 @@ async fn main() {
 
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![text()],
+            commands: vec![commands::text()],
             ..Default::default()
         })
         .setup(|ctx, _ready, framework| {
             Box::pin(async move {
                 poise::builtins::register_in_guild(ctx, &framework.options().commands, serenity::GuildId::new(936376273138245652)).await?;
-                Ok(Data {
+                Ok(DataDiscord {
                     generation_ai,
                 })
             })
