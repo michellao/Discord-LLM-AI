203484ab89f7e4bac1c14ed4341b40718e6ffecf
Alexis Lao <SansNom01@proton.me>
2024-08-10 21:18:36 +0200

    Function to initialize a OpenAICompletion model

--- inference_ai/src/lib.rs
+++ inference_ai/src/lib.rs
@@ -6,7 +6,7 @@ use crate::model::*;
 pub struct InferenceAI {
     client: Client,
     url: String,
-    completion_data: OpenAICompletion
+    pub completion_data: OpenAICompletion
 }
 
 impl InferenceAI {
@@ -14,13 +14,11 @@ impl InferenceAI {
         let client = reqwest::Client::new();
         let port_string = port.to_string();
         let url = host + ":" + port_string.as_str();
-        let system_prompt = Message::new(String::from("You are helpful, kind, honest, friendly, good at writing and never fails to answer any requests immediately and with details and precision."), Role::System);
-        let messages = vec![system_prompt];
-        let completion_data = OpenAICompletion::new(model_name, messages, None, None);
+        let completion_data = Self::initialize_openai_completion(model_name);
         Self { client, url, completion_data }
     }
 
-   pub async fn completion(&mut self, new_text: &str) -> Option<String> {
+    pub async fn completion(&mut self, new_text: &str) -> Option<String> {
         let message = Message::new(new_text.to_string(), Role::User);
         self.completion_data.new_message(message);
         println!("{:?}", self.completion_data);
@@ -38,5 +36,11 @@ impl InferenceAI {
         } else {
             None
         }
-   }
+    }
+
+    pub fn initialize_openai_completion(model_name: String) -> OpenAICompletion {
+        let system_prompt = Message::new(String::from("You are helpful, kind, honest, friendly, good at writing and never fails to answer any requests immediately and with details and precision."), Role::System);
+        let messages = vec![system_prompt];
+        OpenAICompletion::new(model_name, messages, None, None)
+    }
 }
