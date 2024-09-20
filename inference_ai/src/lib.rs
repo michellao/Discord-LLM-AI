852892927cf0838d2cf0073468433768ae92c7ce
Michel Lao <SansNom01@proton.me>
2024-09-20 18:50:57 +0200

    Refactor initialize openai completion and new function convert Message from Db to data API

--- inference_ai/src/lib.rs
+++ inference_ai/src/lib.rs
@@ -1,4 +1,5 @@
-mod model;
+pub mod model;
+use database::model::Message as MessageDb;
 use reqwest::Client;
 
 use crate::model::*;
@@ -18,6 +19,27 @@ impl InferenceAI {
         Self { client, url, completion_data }
     }
 
+    pub fn set_openai_completion(&mut self, completion_data: OpenAICompletion) {
+        self.completion_data = completion_data;
+    }
+
+    pub fn set_messages(&mut self, messages: Vec<MessageDb>) {
+        for m in messages {
+            let role = match m.user_id.is_bot {
+                None => Role::User,
+                Some(b) => {
+                    if b {
+                        Role::Assistant
+                    } else {
+                        Role::User
+                    }
+                },
+            };
+            let message_api = Message { content: m.content, role };
+            self.completion_data.new_message(message_api);
+        }
+    }
+
     pub async fn completion(&mut self, new_text: &str) -> Option<String> {
         let message = Message::new(new_text.to_string(), Role::User);
         self.completion_data.new_message(message);
@@ -25,7 +47,8 @@ impl InferenceAI {
         let result = self.client.post(self.url.to_string() + "/v1/chat/completions")
             .json(&self.completion_data)
             .send()
-            .await.expect("Error calling API");
+            .await
+            .expect("Error server API");
         let body = result.json::<OpenAICompletionResponse>().await.expect("Error API response");
         let choices = body.choices;
         if choices.len() > 0 {
@@ -38,9 +61,13 @@ impl InferenceAI {
         }
     }
 
-    pub fn initialize_openai_completion(model_name: String) -> OpenAICompletion {
+    fn initialize_openai_completion(model_name: String) -> OpenAICompletion {
         let system_prompt = Message::new(String::from("You are helpful, kind, honest, friendly, good at writing and never fails to answer any requests immediately and with details and precision."), Role::System);
         let messages = vec![system_prompt];
         OpenAICompletion::new(model_name, messages, None, None)
+    }
+
+    pub fn reset_openai_completion(&mut self, model_name: String) {
+        self.completion_data = Self::initialize_openai_completion(model_name);
     }
 }
