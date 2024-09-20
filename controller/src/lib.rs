82c8d394e815bdb7b31e4967f5074af7cf24f3bc
Michel Lao <SansNom01@proton.me>
2024-09-20 17:44:30 +0200

    Remove mut on database and get an user by discord id

--- controller/src/lib.rs
+++ controller/src/lib.rs
@@ -1,21 +1,35 @@
 use database::{ model::{Message, User}, Database };
 
 pub struct UserController<'a> {
-    database: &'a mut Database
+    database: &'a Database
 }
 pub struct MessageController<'a> {
-    database: &'a mut Database
+    database: &'a Database
 }
+
+impl<'a> UserController<'a> {
+    pub async fn get_by_discord_id(&self, discord_id: i64) -> Option<User> {
+        let user = User { discord_id, ..Default::default() };
+        let sql = self.database.select_from(&user, "discord_id");
+        let result: Option<User> = sqlx::query_as(&sql)
+            .bind(&user.discord_id)
+            .fetch_optional(&self.database.conn)
+            .await
+            .expect("Error connection database");
+        result
+    }
+}
+
 pub trait Controller<'a> {
     type ModelController;
-    fn new(database: &'a mut Database) -> Self;
+    fn new(database: &'a Database) -> Self;
     fn get(&self, id: i64) -> impl std::future::Future<Output = Option<Self::ModelController>> + Send;
 }
 
 impl<'a> Controller<'a> for UserController<'a> {
     type ModelController = User;
 
-    fn new(database: &'a mut Database) -> Self {
+    fn new(database: &'a Database) -> Self {
         UserController {
             database
         }
@@ -26,7 +40,7 @@ impl<'a> Controller<'a> for UserController<'a> {
         let sql = self.database.select_from_id(&object);
         let result: Option<Self::ModelController> = sqlx::query_as(&sql)
             .fetch_optional(&self.database.conn)
-            .await.expect("Database error");
+            .await.expect("Error connection database");
         result
     }
 }
@@ -34,7 +48,7 @@ impl<'a> Controller<'a> for UserController<'a> {
 impl<'a> Controller<'a> for MessageController<'a> {
     type ModelController = Message;
 
-    fn new(database: &'a mut Database) -> Self {
+    fn new(database: &'a Database) -> Self {
         MessageController {
             database
         }
