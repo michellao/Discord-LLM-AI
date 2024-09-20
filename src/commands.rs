5989b8e9919e856bd59c0aeb1291e5e28489bc66
Michel Lao <SansNom01@proton.me>
2024-09-20 18:52:43 +0200

    Insertion to database on completion

--- src/commands.rs
+++ src/commands.rs
@@ -1,4 +1,32 @@
+use controller::{Controller, UserController};
+use database::{model::{Message, User}, Database};
 use crate::{Context,Error};
+
+async fn get_from_database_or_create_user(user_controller: &UserController<'_>, database: &Database, author: &serenity::model::prelude::User) -> User {
+    let discord_id = i64::try_from(author.id.get()).expect("Overflow i64 on discord_id");
+    let author_user = match user_controller.get_by_discord_id(discord_id).await {
+        None => {
+            let mut u = User { is_bot: Some(author.bot), discord_id, ..Default::default() };
+            let _r = database.insert(&mut u).await;
+            u
+        }
+        Some(a) => a
+    };
+    author_user
+}
+
+async fn get_from_database_or_create_bot_user(user_controller: &UserController<'_>, database: &Database, ctx: Context<'_>) -> User {
+    let discord_id = i64::try_from(ctx.cache().current_user().id.get()).expect("Overflow i64 on discord_id");
+    let user = match user_controller.get_by_discord_id(discord_id).await {
+        None => {
+            let mut u = User { is_bot: Some(ctx.cache().current_user().bot), discord_id, ..Default::default() };
+            database.insert(&mut u).await;
+            u
+        }
+        Some(a) => a
+    };
+    user
+}
 
 /// Response by an AI
 #[poise::command(slash_command)]
@@ -13,10 +41,24 @@ pub async fn text(
     let handle_generating_message = ctx.send(reply).await?;
 
     let generation_ai = &ctx.data().generation_ai;
-    let response = generation_ai.generate(prompt).await;
+
+    let database = &ctx.data().database;
+    let user_controller = UserController::new(database);
+    let author_user = ctx.author();
+    let author_bot = get_from_database_or_create_bot_user(&user_controller, database, ctx).await;
+    let author_model_user = get_from_database_or_create_user(&user_controller, database, author_user).await;
+    println!("{:?}", author_model_user);
+
+    let response = generation_ai.generate(&prompt).await;
+
+    let mut message_from_user = Message { user_id: author_model_user, content: prompt, ..Default::default() };
+    database.insert(&mut message_from_user).await;
+
     println!("Complete response: {}", response);
     let lenght_response = response.len();
     let character_limit = 2000;
+    let mut message_from_bot = Message { user_id: author_bot, content: response.to_owned(), ..Default::default() };
+    database.insert(&mut message_from_bot).await;
     let mut complete_response: Vec<&str> = vec![];
     if lenght_response > character_limit {
         let mut response_to_divide = response.as_str();
